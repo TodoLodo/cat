8ce4cc0cdf48a900c0773cc39f766233cc03a786
Todo Lodo <me@todolodo.xyz>
2022-11-02 16:32:04 +0530

    clean
    Signed-off-by: Todo Lodo <me@todolodo.xyz>

---
+++ src/cat.py
@@ -0,0 +1,39 @@
+import sys
+
+
+class Cat:
+    def __init__(self, args):
+        self.numbered = False
+        self.ending = False
+
+        self.n = 0
+
+        if "-n" in args:
+            self.numbered = True
+            args.remove("-n")
+        if "-E" in args:
+            self.ending = True
+            args.remove("-E")
+
+        if len(args) == 0:
+            self.interactive()
+
+        for file in args:
+            with open(file, "r") as f:
+                for line in f.readlines():
+                    self.display(line.strip("\n"))
+                    self.n += 1
+
+
+    def interactive(self):
+        while True:
+            user = input()
+            self.display(user)
+            self.n += 1
+
+    def display(self, line):
+        print(f"{f'{self.n} ' if self.numbered else ''}{line}{'$' if self.ending else ''}")
+
+
+if __name__ == '__main__':
+    Cat(sys.argv[1:])
