3eb0a6f6c76d092592cb23802c7844d5206f58e1
Todo Lodo <me@todolodo.xyz>
2022-11-05 00:33:28 +0530

    update

--- compile.bat
+++ compile.bat
@@ -1,5 +1,4 @@
-pyinstaller -F scripts/cat.py -n cat --icon=ico/cat.ico --clean --distpath ./bin
-pyinstaller -F scripts/cat_setup.py -n cat_setup --icon=ico/cat.ico -w --uac-admin --clean --distpath ./
+pyinstaller -F src/cat.py -n cat --icon=ico/cat.ico --clean --distpath ./bin
 rmdir /s /q build
 del cat.spec cat_setup.spec
 pause
